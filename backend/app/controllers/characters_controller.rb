6e753b8a65b422de0a19dd6366c9b8fee85e6aa1
popkorn96 <mpaloma2014@gmail.com>
2020-11-16 15:23:38 -0600

    added characters controller

--- backend/app/controllers/characters_controller.rb
+++ backend/app/controllers/characters_controller.rb
@@ -1,2 +1,30 @@
 class CharactersController < ApplicationController
+    before_action :character, only => [:show, :update, :destroy]
+    def index 
+        render json: Character.all
+    end
+    def create 
+        render json: Character.create(character_params)
+    end
+    def show
+        render json: @character
+    end
+    def update
+        @character.update(character_params)
+            if @character.save
+                render json: @character
+            else 
+                render json: {errors: @character.errors.full_messages}
+        end
+    end
+    def destroy
+        render json: @character.destroy
+    end
+    private
+    def character
+        @character = Character.find(params[:id])
+    end
+    def character_params
+        params.permit(:name, :age, :favorite_color)
+    end
 end
