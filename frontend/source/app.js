f1c8b9bbbfd339aaf3b142356f444bd96242f442
popkorn96 <mpaloma2014@gmail.com>
2020-11-16 20:47:28 -0600

    adding js classes

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -1,13 +1,28 @@
 const BASE_URL = "http://localhost:3000";
 const CHARACTERS_URL = `${BASE_URL}/characters`;
 const GIFTS_URL = `${BASE_URL}/gifts`;
+
 class App{
     static fetchOneCharacter(id) {
         return fetch(CHARACTERS_URL)
         .then(response => response.json())
+        .then(data => {
+            postFetchCharacter(data)
+        })
     }
-    static fetchOneTicket(id) {
+    static fetchOneGift(id) {
         return fetch(GIFTS_URL)
         .then(response => response.json())
+        .then(data => {
+            postFetchGift(data)
+        })
+    }
+    static postFetchCharacter(name, age, favorite_color){
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            age: age,
+            favorite_color: favorite_color
+        }),
     }
 }
---
+++ frontend/source/gift.js
@@ -0,0 +1,7 @@
+class {
+    constructor(name, price, img_url){
+        this.name = name;
+        this.price = price;
+        this.img_url = img_url;
+    }
+}
