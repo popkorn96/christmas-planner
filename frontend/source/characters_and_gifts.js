56273e455fc7455b716c37997a4daf025dc00411
popkorn96 <mpaloma2014@gmail.com>
2020-12-04 14:07:44 -0600

    added sort button

--- frontend/source/characters_and_gifts.js
+++ frontend/source/characters_and_gifts.js
@@ -30,8 +30,54 @@ class CharactersAndGifts{
         })
     }
     renderCharacters(){
+        const alph = document.getElementById('alph')
+        alph.innerText = `Alphabetize!`
+        alph.addEventListener('click', this.alphabatize.bind(this))
         this.characters.map( character => 
             character.renderCard()).join('')
+    }
+    alphabatize(){
+        const alph = document.getElementById('alph')
+        alph.removeEventListener('click', this.alphabatize)
+        alph.innerText = `Sort By Age!`
+        alph.addEventListener('click', this.sortByAge.bind(this))
+        let charactersContainer = document.getElementById('character-list')
+        charactersContainer.innerHTML = ""
+        this.characters.sort(function(a, b){
+            var x = a.name.toLowerCase();
+            var y = b.name.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+        }).map( character => character.renderCard()).join('')
+    }
+    sortByAge(){
+        this.charactersContainer.innerHTML = ""
+        let alph = document.getElementById('alph')
+        alph.removeEventListener('click', this.sortByAge)
+        alph.innerText = `Sort by Date Created!`
+        alph.addEventListener('click', this.sortByDateCreated.bind(this))
+        this.characters.sort(function(a, b){
+            var x = a.age;
+            var y = b.age;
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0
+        }).map( character => character.renderCard()).join('')
+    }
+    sortByDateCreated(){
+        this.charactersContainer.innerHTML = ""
+        let alph = document.getElementById('alph')
+        alph.removeEventListener('click', this.sortByDateCreated)
+        alph.innerText = `Alphabetize!`
+        alph.addEventListener('click', this.alphabatize.bind(this))
+        this.characters.sort(function(a, b){
+            var x = a.id;
+            var y = b.id;
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0
+        }).map( character => character.renderCard()).join('')
     }
     static clearForms(){
         let newCharForm = document.getElementById('new-character-form')
--- frontend/source/index.js
+++ frontend/source/index.js
@@ -6,6 +6,7 @@ console.log('helloooo')
 document.addEventListener('DOMContentLoaded', init)
 const addCharacter = document.getElementById('new-character-button')
 const clearFormsButton = document.getElementById('clear-forms-button')
+
 clearFormsButton.setAttribute('class', 'button2')
 function init(){
     addCharacter.addEventListener('click', createNewCharacter)
